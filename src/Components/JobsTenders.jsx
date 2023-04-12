3f715f8dd8069119ee8c74770c946859d32c849d
kiwele <musakiwele@gmail.com>
2023-04-12 06:27:49 +0300

    modified tender page

--- src/Components/JobsTenders.jsx
+++ src/Components/JobsTenders.jsx
@@ -2,8 +2,37 @@ import React from "react";
 import Header from "../images/background.jpg";
 import author4 from "../images/application.jpg";
 import Movieheading from "./Movieheading";
+import { useRef } from "react";
+import emailjs from "@emailjs/browser";
 
 function JobsTenders() {
+
+
+  const form = useRef();
+
+  const sendEmail = (e) => {
+    e.preventDefault();
+
+    // emailjs.sendForm(process.env.YOUR_SERVICE_ID, process.env.YOUR_TEMPLATE_ID, form.current, process.env.YOUR_PUBLIC_KEY)
+    emailjs
+      .sendForm(
+        "service_vzo9vab",
+        "template_11mr5oq",
+        form.current,
+        "qKDi3bw3jvJiEgp8J"
+      )
+      .then(
+        (result) => {
+          console.log(result.text);
+        },
+        (error) => {
+          console.log(error);
+        }
+      );
+  };
+
+
+
   return (
     <div>
       <div className="head-text">
@@ -50,31 +79,43 @@ function JobsTenders() {
           {/* <span className="success-message">{successMessage}</span> */}
         </div>
         {/* <div className="container"> */}
+
+        <form ref={form} onSubmit={sendEmail}>
         <div class="form-floating mb-3 g-5">
             <div className="row mx-5">
               <div className="col-lg-6 col-xm-12">
                 <input
                   type="text"
-                  name="name"
+                  name="user_name"
                   className="form-control"
                   placeholder="Full Name"
                 />
                 <input
-                  type="text"
+                  type="email"
+                  name="from_name"
                   // label="email"
                   className="form-control"
                   placeholder="Email"
                 />
                 <input
-                  type="text"
+                  type="number"
+                  name="phone"
                   // label="phone"
                   className="form-control"
                   placeholder="Phone number"
+                />
+                <input
+                  type="file"
+                  // label="phone"
+                  name = "my_file"
+                  className="form-control"
+                  // placeholder="Phone number"
                 />
               </div>
               <div className="col-lg-6 col-xs-12">
                 <textarea
                   type="text"
+                  name="message"
                   // label="description"
                   className="form-control"
                   placeholder="Write your short Message"
@@ -85,8 +126,15 @@ function JobsTenders() {
               </div>
             </div>
           </div>
+
+          </form>
+
+
         {/* </div> */}
       </div>
+
+
+
       <div className="py-5 services" id="about">
         <div className="row">
           <div className="col-lg-4 col-xm-12 about-text mt-5">
